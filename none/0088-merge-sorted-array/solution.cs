a021d846ce83badd9af5ab7cb7ea45e800a51bc3
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-01-21 19:28:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47.3 MB (25.19%)

---
+++ none/0088-merge-sorted-array/solution.cs
@@ -0,0 +1,34 @@
+public class Solution 
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int endIdx = nums1.Length - 1;
+
+        if (n == 0) return;
+
+        while(n > 0 && m > 0)
+        {
+            if (nums2[n-1] >= nums1[m-1])
+            {
+                nums1[endIdx] = nums2[n-1];
+                n--;
+            }
+
+            else
+            {
+                nums1[endIdx] = nums1[m-1];
+                m--;
+            }
+
+            endIdx--;
+        }
+
+        while (n > 0)
+        {
+            nums1[endIdx] = nums2[n-1];
+
+            n--;
+            endIdx--;
+        }
+    }
+}
