4e08ba114a3c93ab04acb881f2bf98ed5c206df6
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-01-23 18:36:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 51.1 MB (56.06%)

---
+++ none/0026-remove-duplicates-from-sorted-array/solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+        int lastUniqueIdx = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[lastUniqueIdx])
+                nums[++lastUniqueIdx] = nums[i];
+        }
+
+        return ++lastUniqueIdx;
+    }
+}
