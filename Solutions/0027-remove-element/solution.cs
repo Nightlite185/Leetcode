4199f8b8fd9e75b4f92d8c1694a1d9b23192b673
夜光 <cieplucharafal@gmail.com>
2026-02-04 00:24:20 +0100

    fix: moved leetcode files from 'none' to Solutions folder

---
+++ Solutions/0027-remove-element/solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) 
+    {
+        int writeIdx = 0;
+        int k = 0;
+
+        for(int readIdx = 0; readIdx < nums.Length; readIdx++)
+        {
+            if (nums[readIdx] != val)
+            {
+                nums[writeIdx] = nums[readIdx];
+                writeIdx++;
+            }
+        }
+
+        return writeIdx;
+    }
+}
