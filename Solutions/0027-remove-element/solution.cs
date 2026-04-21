14ddbb097dfbba61470067db55537a2d6a4abb08
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-04-21 21:13:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.8 MB (59.91%)

--- Solutions/0027-remove-element/solution.cs
+++ Solutions/0027-remove-element/solution.cs
@@ -1,8 +1,8 @@
-public class Solution {
+public class Solution 
+{
     public int RemoveElement(int[] nums, int val) 
     {
         int writeIdx = 0;
-        int k = 0;
 
         for(int readIdx = 0; readIdx < nums.Length; readIdx++)
         {
