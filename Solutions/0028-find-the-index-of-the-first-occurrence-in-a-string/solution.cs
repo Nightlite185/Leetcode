0b4202096754a29451e168a5186cba7460ecb83a
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-04-26 16:58:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (52.60%)

---
+++ Solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) 
+    {
+        int lastNeedleIdx = needle.Length - 1;
+        int needleIdx = 0;
+
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needle[needleIdx])
+            {
+                if (needleIdx == lastNeedleIdx)
+                    return i - needleIdx;
+                
+                needleIdx++;
+
+                continue;
+            }
+
+            i -= needleIdx;
+            needleIdx = 0;
+        }
+
+        return -1;
+    }
+}
