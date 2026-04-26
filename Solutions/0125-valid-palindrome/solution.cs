81f9ba338190875ec5f59fb72a6c993ba09403eb
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-04-26 15:42:33 +0000

    [LeetCode Sync] Runtime - 22 ms (38.32%), Memory - 45.8 MB (34.51%)

---
+++ Solutions/0125-valid-palindrome/solution.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        string cleaned = new([..s.Where(char.IsLetterOrDigit)]);
+        cleaned = cleaned.ToLowerInvariant();
+
+        int j = cleaned.Length;
+
+        for (int i = 0; i < cleaned.Length; i++)
+        {
+            j--;
+
+            if (cleaned[i] != cleaned[j])
+                return false;
+        }
+
+        return true;
+    }
+}
