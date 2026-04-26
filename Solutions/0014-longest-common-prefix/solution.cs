1522e3773c464ad35cca87a6ddaba487037d1eae
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-04-26 18:42:19 +0000

    [LeetCode Sync] Runtime - 18 ms (5.05%), Memory - 45 MB (8.23%)

---
+++ Solutions/0014-longest-common-prefix/solution.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strings)
+    {
+        string firstStr = strings.FirstOrDefault() ?? "";
+        
+        if (strings.Length == 1)
+            return firstStr;
+
+        for (int i = 0; true; i++)
+        {
+            char prev = firstStr.ElementAtOrDefault(i);
+
+            if (prev == default(char))
+                return firstStr[..i];
+
+            foreach(string str in strings.Skip(1))
+            {
+                char c = str.ElementAtOrDefault(i);
+                
+                if (c != prev)
+                    return firstStr[..i];
+            }
+        }
+    }
+}
