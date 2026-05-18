30f5f8eaa28fc037f6a3da2dd123b82e0e8c8e33
夜光 <cieplucharafal@gmail.com>
2026-05-18 18:41:44 +0200

    added a more-optimized solution to max depth of binary tree problem.

---
+++ Solutions/104-maximum-depth-of-binary-tree/max-depth-bin-tree_v2.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public int MaxDepth(TreeNode? root)
+    {
+        if (root is null) return 0;
+
+        int left = MaxDepth(root.left);
+        int right = MaxDepth(root.right);
+
+        return Math.Max(left, right) + 1;
+    }
+}
