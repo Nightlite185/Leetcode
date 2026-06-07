02521516382091073d34746af62357678141a082
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-06-07 17:16:10 +0200

    Time: 1 ms (100.00%) | Memory: 45 MB (32.08%) - LeetSync

--- Solutions/1547-destination-city/destination-city.cs
+++ Solutions/1547-destination-city/destination-city.cs
@@ -2,19 +2,15 @@ public class Solution
 {
     public string DestCity(IList<IList<string>> paths)
     {
-        HashSet<string> notDest = [];
-        HashSet<string> allCities = [];
+        HashSet<string> notDest = [], potentialDest = [];
 
         foreach(var edge in paths)
         {
-            (string? a, string? b) = (edge[0], edge[1]);
-            allCities.Add(a); allCities.Add(b);
-
-            notDest.Add(a);
+            notDest.Add(edge[0]);
+            potentialDest.Add(edge[1]);
         }
 
-        allCities.ExceptWith(notDest);
-
-        return allCities.Single();
+        potentialDest.ExceptWith(notDest);
+        return potentialDest.Single();
     }
 }
