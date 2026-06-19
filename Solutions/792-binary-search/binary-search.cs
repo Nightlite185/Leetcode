c3baf0cb275ee759d9d3770f512b1cf35a5495d1
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-06-19 18:28:37 +0200

    Time: 0 ms (100.00%) | Memory: 53.3 MB (28.62%) - LeetSync

---
+++ Solutions/792-binary-search/binary-search.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length-1;
+
+        while (left <= right)
+        {
+            int mid = left + (right-left) / 2;
+            int num = nums[mid];
+
+            if (num == target)
+                return mid;
+
+            else if (num < target)
+                left = mid + 1;
+
+            else right = mid - 1;
+        }
+
+        return -1;
+    }
+}
