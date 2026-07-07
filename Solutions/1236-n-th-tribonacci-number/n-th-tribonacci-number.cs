c8a0a0ed1307f44d7d7d58a6b13a7b4e1f3f2f2e
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-07-07 22:34:42 +0200

    Time: 0 ms (100.00%) | Memory: 28.9 MB (57.14%) - LeetSync

---
+++ Solutions/1236-n-th-tribonacci-number/n-th-tribonacci-number.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int Tribonacci(int n)
+    {
+        if (n == 0) return 0;
+        if (n is 1 or 2) return 1;
+        
+        var memo = new int[n+1];
+        memo[1] = 1; memo[2] = 1;
+        
+        for (int i = 3; i <= n; i++)
+        {
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3];
+        }
+
+        return memo[n];
+    }
+}
