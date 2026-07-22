c1f6d3ee65a4dcd87959b5b877b2303b5a5c1886
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-07-22 17:30:39 +0200

    Time: 36 ms (13.33%) | Memory: 60.7 MB (98.10%) - LeetSync

---
+++ Solutions/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cs
@@ -0,0 +1,39 @@
+public class Solution
+{
+    public int MaxProfit(int[] prices, int fee)
+    {
+        int n = prices.Length;
+        var memo = new int?[n, 2];
+
+        int dp(int i, bool hold)
+        {
+            if (i >= n) return 0;
+            if (memo[i, ToInt(hold)] is int m) return m;
+
+            int res = 0;
+
+            if (!hold)
+            {
+                // either buy now and go to next i OR skip and go to next
+                res = Math.Max(
+                    -prices[i] + dp(i+1, true),
+                    dp(i+1, false));
+            }
+
+            else
+            {
+                res = Math.Max(
+                    prices[i] - fee + dp(i+1, false),
+                    dp(i+1, true)
+                );
+            }
+
+            memo[i, ToInt(hold)] = res;
+            return res;
+        }
+
+        return dp(0, false);
+    }
+
+    private static int ToInt(bool b) => b ? 1 : 0;
+}
