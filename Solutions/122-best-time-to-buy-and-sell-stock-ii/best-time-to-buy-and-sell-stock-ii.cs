30e80d8efcdf39854409abe6309eae3cd2228d4f
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-08-24 18:53:49 +0200

    Time: 0 ms (100.00%) | Memory: 44.3 MB (28.20%) - LeetSync

--- Solutions/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
+++ Solutions/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
@@ -2,31 +2,16 @@ public class Solution
 {
     public int MaxProfit(int[] prices)
     {
-        int n = prices.Length;
+        int max = 0;
 
-        var memo = new int?[n,2];
-        
-        int dp(int i, bool holding)
+        for (int i = 1; i < prices.Length; i++)
         {
-            if (i == n) return 0;
-            
-            if (memo[i, Convert.ToInt32(holding)] is int m)
-                return m;
+            int profit = prices[i] - prices[i-1];
 
-            int ans;
-
-            if (holding) ans = Math.Max(
-                prices[i] + dp(i+1, false),
-                dp(i+1, true));
-
-            else ans = Math.Max(
-                dp(i+1, true) - prices[i],
-                dp(i+1, false));
-
-            memo[i, Convert.ToInt32(holding)] = ans;
-            return ans;
+            if (profit > 0)
+                max += profit;
         }
 
-        return dp(0, false);
+        return max;
     }
 }
