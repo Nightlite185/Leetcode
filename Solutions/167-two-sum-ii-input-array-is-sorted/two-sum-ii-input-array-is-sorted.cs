3cc277563c84eeebe5c3841896ee0f61a9c448fe
꧁.ೃ࿔⋆『夜光』⋆.ೃ࿔꧂ <cieplucharafal@gmail.com>
2026-08-29 20:21:49 +0200

    Time: 2 ms (18.71%) | Memory: 49.3 MB (71.76%) - LeetSync

---
+++ Solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length-1;
+
+        while (l < r)
+        {
+            int sum = nums[l] + nums[r];
+            
+            if (sum > target)
+                r--;
+
+            else if (sum < target)
+                l++;
+
+            else if (sum == target)
+                return [l+1, r+1];
+        }
+
+        return [];
+    }
+}
